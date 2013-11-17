94f06b9508c7c4dc6b531b5ad62c932a30f6afe2
Thomas Chojecki <info@rayman2200.de>
2013-11-17 21:54:19 +0100

    add some docu and license header

--- drop-converter-app/src/main/java/de/drop_converter/components/JPluginConfiguration.java
+++ drop-converter-app/src/main/java/de/drop_converter/components/JPluginConfiguration.java
@@ -1,22 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package de.drop_converter.components;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-import de.drop_converter.PluginWrapper;
 import de.drop_converter.PluginHandler;
+import de.drop_converter.PluginWrapper;
 
+/**
+ * Configure the installed plugins.
+ * 
+ * @author Thomas Chojecki
+ */
 public class JPluginConfiguration extends JPanel
 {
   private final PluginHandler pluginHandler;
 
   private JList<PluginWrapper> pluginList;
 
+  private JTable pluginOverviewe = new JTable();
+
   public JPluginConfiguration(PluginHandler pluginHandler)
   {
     this.pluginHandler = pluginHandler;
 
     add(new JLabel("Dummy plugin configuration."));
+
+    // Set<PluginWrapper> plugins = pluginHandler.getPlugins();
+
+    add(new JScrollPane(pluginOverviewe));
+
   }
 }
