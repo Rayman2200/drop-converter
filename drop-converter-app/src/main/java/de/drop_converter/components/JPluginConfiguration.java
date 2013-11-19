97253d412006364e9e9a1de8b518921f5f7306ec
Thomas Chojecki <info@rayman2200.de>
2013-11-19 21:38:24 +0100

    add new plugin configuration. - overview with the loaded plugins - tabs with specific plugin configuration

--- drop-converter-app/src/main/java/de/drop_converter/components/JPluginConfiguration.java
+++ drop-converter-app/src/main/java/de/drop_converter/components/JPluginConfiguration.java
@@ -3,37 +3,72 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package de.drop_converter.components;
 
+import java.awt.BorderLayout;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import de.drop_converter.PluginHandler;
 import de.drop_converter.PluginWrapper;
+import de.drop_converter.listener.PluginListener;
 
 /**
  * Configure the installed plugins.
  * 
  * @author Thomas Chojecki
  */
-public class JPluginConfiguration extends JPanel
+public class JPluginConfiguration extends JPanel implements PluginListener
 {
   private final PluginHandler pluginHandler;
 
-  private JList<PluginWrapper> pluginList;
+  private final DefaultTableModel model;
 
-  private JTable pluginOverviewe = new JTable();
+  private final JTable pluginOverviewe;
 
   public JPluginConfiguration(PluginHandler pluginHandler)
   {
     this.pluginHandler = pluginHandler;
+    pluginHandler.addPluginListener(this);
+    setLayout(new BorderLayout());
+    add(new JLabel("Dummy plugin configuration."), BorderLayout.NORTH);
 
-    add(new JLabel("Dummy plugin configuration."));
+    Set<PluginWrapper> plugins = pluginHandler.getPlugins();
+    Iterator<PluginWrapper> it = plugins.iterator();
 
-    // Set<PluginWrapper> plugins = pluginHandler.getPlugins();
+    String[] t = new String[] { "Name", "Description", "Version", "Website", "Author", "Email", "Enabled?" };
+    model = new DefaultTableModel(t, 0);
+    
+    while (it.hasNext()) {
+      addPlugin((PluginWrapper) it.next());
+    }
 
-    add(new JScrollPane(pluginOverviewe));
+    pluginOverviewe = new JTable(model);
+    add(new JScrollPane(pluginOverviewe), BorderLayout.CENTER);
+
+  }
+
+  private void addPlugin(PluginWrapper p)
+  {
+    Object[] v = new Object[] { p.getPluginName(), p.getPluginDescription(), p.getPluginVersion(), 
+                                p.getPluginWebsite(), p.getAuthorName(),p.getAuthorEmail(), new JCheckBox("", true) };
+    model.addRow(v);
+  }
+
+  @Override
+  public void addedPlugin(PluginWrapper plugin)
+  {
+    addPlugin(plugin);
+  }
+
+  @Override
+  public void removedPlugin(PluginWrapper plugin)
+  {
 
   }
 }
